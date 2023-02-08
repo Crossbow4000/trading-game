e3a8c72fed05cdc8732e4d809fcac4656a320932
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-02-08 09:01:39 -0600

    2 files added

---
+++ src/pages/home-page/components/crafting/crafting.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import '../../../../main.css';
+import './crafting.css'
+
+
+
+export default function Crafting() {
+
+}
+
+function CraftingCard(props) {
+  return(
+    <div className={"grid | item-card"}>
+      <h1 className={"item-card-title"}>{props.name}</h1>
+      <h2 className={`item-card-rarity item-card-rarity-${props.rarity}`}>{props.rarity}</h2>
+      <img className={"item-card-image"} src={props.image}/>
+      <p className={"item-card-quantity"}>{props.quantity}</p>
+    </div>
+  )
+}
